980ab79cdf564880f723e0735614475fec546b9b
Glenn Nagel <glenn@mercury-wireless.com>
2018-02-07 10:55:28 -0500

    Flushing out exact tests

--- __tests__/exact.test.js
+++ __tests__/exact.test.js
@@ -11,9 +11,33 @@ jest.mock("commander", () => ({
 }));
 
 describe("generate flow types", () => {
-  describe("with --exact", () => {
-    it("should generate expected flow types", () => {
-      const file = path.join(__dirname, "__mocks__/swagger-v2.yaml");
+  describe("Open API V2: with --exact", () => {
+    it.skip("should generate expected flow types from swagger.yml", () => {
+      const file = path.join(__dirname, "__mocks__/exact/swagger.yaml");
+      const expected = path.join(
+        __dirname,
+        "__mocks__/exact/expected.yaml.flow.js"
+      );
+      const expectedString = fs.readFileSync(expected, "utf8");
+      const output = generator(file);
+      expect(output).toEqual(expectedString);
+    });
+
+    it.skip("should generate expected flow types from swagger.json", () => {
+      const file = path.join(__dirname, "__mocks__/exact/swagger.json");
+      const expected = path.join(
+        __dirname,
+        "__mocks__/exact/expected.json.flow.js"
+      );
+      const expectedString = fs.readFileSync(expected, "utf8");
+      const output = generator(file);
+      expect(output).toEqual(expectedString);
+    });
+  });
+
+  describe("Open API V3: with --exact", () => {
+    it("should generate expected flow types from swagger-v2.yml", () => {
+      const file = path.join(__dirname, "__mocks__/exact/swagger-v2.yaml");
       const expected = path.join(
         __dirname,
         "__mocks__/exact/expected-v2.yaml.flow.js"
@@ -24,7 +48,7 @@ describe("generate flow types", () => {
     });
 
     it("should generate expected flow types from swagger-v2.json", () => {
-      const file = path.join(__dirname, "__mocks__/swagger-v2.json");
+      const file = path.join(__dirname, "__mocks__/exact/swagger-v2.json");
       const expected = path.join(
         __dirname,
         "__mocks__/exact/expected-v2.json.flow.js"
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,28 +2,48 @@ import fs from "fs";
 import path from "path";
 import { FlowTypeGenerator, generator } from "../src/index";
 
-jest.mock('commander', () => {
-  return {
-    checkRequired: false,
-    arguments: jest.fn().mockReturnThis(),
-    option: jest.fn().mockReturnThis(),
-    action: jest.fn().mockReturnThis(),
-    parse: jest.fn().mockReturnThis(),
-  }
-});
+jest.mock("commander", () => ({
+  checkRequired: false,
+  arguments: jest.fn().mockReturnThis(),
+  option: jest.fn().mockReturnThis(),
+  action: jest.fn().mockReturnThis(),
+  parse: jest.fn().mockReturnThis()
+}));
 
 describe("generate flow types", () => {
-  describe("without --check-required", () => {
-    it("should generate expected flow types", () => {
+  describe("Open API V2: without --check-required", () => {
+    it.skip("should generate expected flow types from swagger.yaml", () => {
+      const file = path.join(__dirname, "__mocks__/swagger.yaml");
+      const expected = path.join(__dirname, "__mocks__/expected.yaml.flow.js");
+      const expectedString = fs.readFileSync(expected, "utf8");
+      expect(generator(file)).toEqual(expectedString);
+    });
+
+    it.skip("should generate expected flow types from swagger.json", () => {
+      const file = path.join(__dirname, "__mocks__/swagger.json");
+      const expected = path.join(__dirname, "__mocks__/expected.json.flow.js");
+      const expectedString = fs.readFileSync(expected, "utf8");
+      expect(generator(file)).toEqual(expectedString);
+    });
+  });
+
+  describe("Open API V3: without --check-required", () => {
+    it("should generate expected flow types from swagger-v2.yaml", () => {
       const file = path.join(__dirname, "__mocks__/swagger-v2.yaml");
-      const expected = path.join(__dirname, "__mocks__/expected-v2.yaml.flow.js");
+      const expected = path.join(
+        __dirname,
+        "__mocks__/expected-v2.yaml.flow.js"
+      );
       const expectedString = fs.readFileSync(expected, "utf8");
       expect(generator(file)).toEqual(expectedString);
     });
 
     it("should generate expected flow types from swagger-v2.json", () => {
       const file = path.join(__dirname, "__mocks__/swagger-v2.json");
-      const expected = path.join(__dirname, "__mocks__/expected-v2.json.flow.js");
+      const expected = path.join(
+        __dirname,
+        "__mocks__/expected-v2.json.flow.js"
+      );
       const expectedString = fs.readFileSync(expected, "utf8");
       expect(generator(file)).toEqual(expectedString);
     });
