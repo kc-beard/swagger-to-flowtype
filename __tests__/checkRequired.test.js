3907ec16375e21bca5da24822857960648116b0e
Glenn Nagel <glenn@mercury-wireless.com>
2018-02-07 10:44:37 -0500

    Flushing out enum on root tests

--- __tests__/checkRequired.test.js
+++ __tests__/checkRequired.test.js
@@ -2,24 +2,22 @@ import fs from "fs";
 import path from "path";
 import { FlowTypeGenerator, generator } from "../src/index";
 
-jest.mock("commander", () => {
-  return {
-    checkRequired: true,
-    arguments: jest.fn().mockReturnThis(),
-    option: jest.fn().mockReturnThis(),
-    action: jest.fn().mockReturnThis(),
-    parse: jest.fn().mockReturnThis()
-  };
-});
-
-
+jest.mock("commander", () => ({
+  checkRequired: true,
+  arguments: jest.fn().mockReturnThis(),
+  option: jest.fn().mockReturnThis(),
+  action: jest.fn().mockReturnThis(),
+  parse: jest.fn().mockReturnThis()
+}));
 
 describe("generate flow types", () => {
-
   describe("Open API V2: with --check-required from swagger.yaml", () => {
     it.skip("should generate expected flow types", () => {
       const file = path.join(__dirname, "__mocks__/checkRequired/swagger.yaml");
-      const expected = path.join(__dirname, "__mocks__/checkRequired/expected.yaml.flow.js");
+      const expected = path.join(
+        __dirname,
+        "__mocks__/checkRequired/expected.yaml.flow.js"
+      );
       const expectedString = fs.readFileSync(expected, "utf8");
       const output = generator(file);
       expect(output).toEqual(expectedString);
@@ -27,7 +25,10 @@ describe("generate flow types", () => {
 
     it.skip("should generate expected flow types from swagger.json", () => {
       const file = path.join(__dirname, "__mocks__/checkRequired/swagger.json");
-      const expected = path.join(__dirname, "__mocks__/checkRequired/expected.json.flow.js");
+      const expected = path.join(
+        __dirname,
+        "__mocks__/checkRequired/expected.json.flow.js"
+      );
       const expectedString = fs.readFileSync(expected, "utf8");
       const output = generator(file);
       expect(output).toEqual(expectedString);
@@ -36,20 +37,31 @@ describe("generate flow types", () => {
 
   describe("Open API V3: with --check-required", () => {
     it("should generate expected flow types from swagger-v2.yaml", () => {
-      const file = path.join(__dirname, "__mocks__/checkRequired/swagger-v2.yaml");
-      const expected = path.join(__dirname,"__mocks__/checkRequired/expected-v2.yaml.flow.js");
+      const file = path.join(
+        __dirname,
+        "__mocks__/checkRequired/swagger-v2.yaml"
+      );
+      const expected = path.join(
+        __dirname,
+        "__mocks__/checkRequired/expected-v2.yaml.flow.js"
+      );
       const expectedString = fs.readFileSync(expected, "utf8");
       const output = generator(file);
       expect(output).toEqual(expectedString);
     });
 
     it("should generate expected flow types from swagger-v2.json", () => {
-      const file = path.join(__dirname, "__mocks__/checkRequired/swagger-v2.json");
-      const expected = path.join(__dirname, "__mocks__/checkRequired/expected-v2.json.flow.js");
+      const file = path.join(
+        __dirname,
+        "__mocks__/checkRequired/swagger-v2.json"
+      );
+      const expected = path.join(
+        __dirname,
+        "__mocks__/checkRequired/expected-v2.json.flow.js"
+      );
       const expectedString = fs.readFileSync(expected, "utf8");
       const output = generator(file);
       expect(output).toEqual(expectedString);
     });
   });
-
 });
--- __tests__/enumOnRoot.test.js
+++ __tests__/enumOnRoot.test.js
@@ -2,21 +2,29 @@ import fs from "fs";
 import path from "path";
 import { FlowTypeGenerator, generator } from "../src/index";
 
-jest.mock('commander', () => {
-  return {
-    checkRequired: true,
-    arguments: jest.fn().mockReturnThis(),
-    option: jest.fn().mockReturnThis(),
-    action: jest.fn().mockReturnThis(),
-    parse: jest.fn().mockReturnThis(),
-  }
-});
+jest.mock("commander", () => ({
+  checkRequired: true,
+  arguments: jest.fn().mockReturnThis(),
+  option: jest.fn().mockReturnThis(),
+  action: jest.fn().mockReturnThis(),
+  parse: jest.fn().mockReturnThis()
+}));
 
 describe("generate flow types", () => {
-  describe("parse enum on root element", () => {
+  describe("Open API V2: parse enum on root element", () => {
+    it.skip("should generate expected flow types", () => {
+      const file = path.join(__dirname, "__mocks__/enumOnRoot/enumOnRoot.swagger-v2.yaml");
+      const expected = path.join(__dirname, "__mocks__/enumOnRoot/enumOnRoot.flow.js");
+      const expectedString = fs.readFileSync(expected, "utf8");
+      const output = generator(file);
+      expect(output).toEqual(expectedString);
+    });
+  });
+
+  describe("Open API V3: parse enum on root element", () => {
     it("should generate expected flow types", () => {
-      const file = path.join(__dirname, "__mocks__/enumOnRoot.swagger-v2.yaml");
-      const expected = path.join(__dirname, "__mocks__/enumOnRoot.flow.js");
+      const file = path.join(__dirname, "__mocks__/enumOnRoot/enumOnRoot.swagger-v2.yaml");
+      const expected = path.join(__dirname, "__mocks__/enumOnRoot/enumOnRoot.flow.js");
       const expectedString = fs.readFileSync(expected, "utf8");
       const output = generator(file);
       expect(output).toEqual(expectedString);
