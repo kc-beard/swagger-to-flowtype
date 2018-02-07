ac4d37a35e2c770c23e241a2e047e29de51f8abd
Glenn Nagel <glenn@mercury-wireless.com>
2018-02-07 10:47:02 -0500

    Adding object in array tests

--- __tests__/objectInArrayWithExact.test.js
+++ __tests__/objectInArrayWithExact.test.js
@@ -2,22 +2,44 @@ import fs from "fs";
 import path from "path";
 import { generator } from "../src/index";
 
-jest.mock('commander', () => {
+jest.mock("commander", () => {
   return {
     exact: true,
     checkRequired: true,
     arguments: jest.fn().mockReturnThis(),
     option: jest.fn().mockReturnThis(),
     action: jest.fn().mockReturnThis(),
-    parse: jest.fn().mockReturnThis(),
-  }
+    parse: jest.fn().mockReturnThis()
+  };
 });
 
 describe("generate flow types", () => {
-  describe("parse objct in array", () => {
+  describe("Open API V2: parse objct in array", () => {
+    it.skip("should generate expected flow types", () => {
+      const file = path.join(
+        __dirname,
+        "__mocks__/objectInArray/objectInArray.swagger.yaml"
+      );
+      const expected = path.join(
+        __dirname,
+        "__mocks__/objectInArray/objectInArrayWithExact.flow.js"
+      );
+      const expectedString = fs.readFileSync(expected, "utf8");
+      const output = generator(file);
+      expect(output).toEqual(expectedString);
+    });
+  });
+
+  describe("Open API V3: parse objct in array", () => {
     it("should generate expected flow types", () => {
-      const file = path.join(__dirname, "__mocks__/objectInArray.swagger-v2.yaml");
-      const expected = path.join(__dirname, "__mocks__/objectInArrayWithExact.flow.js");
+      const file = path.join(
+        __dirname,
+        "__mocks__/objectInArray/objectInArray.swagger-v2.yaml"
+      );
+      const expected = path.join(
+        __dirname,
+        "__mocks__/objectInArray/objectInArrayWithExact.flow.js"
+      );
       const expectedString = fs.readFileSync(expected, "utf8");
       const output = generator(file);
       expect(output).toEqual(expectedString);
